ee25d381e787773687e49b6b057235f9098138ce
CaHbI4 <FILlove@mail.ru>
2022-05-12 17:40:40 +0300

    SkyPro

--- hw10/function.py
+++ hw10/function.py
@@ -7,3 +7,37 @@ def load_candidate():
         file = json.load(file)
         return tuple(file)
 
+
+def get_candidates():
+    """ Выведите полный список кандидатов """
+    candidate = ""
+    for candidates in load_candidate():
+        candidate += "Имя кандидата: " + candidates["name"] + " \n" \
+                     "Позиция кандидата: " + candidates["position"] + "\n" \
+                     "Навыки через запятую: " + candidates["skills"] + "\n""\n"
+    return "<h2>" + "<pre>" + candidate + "</pre>" + "<h2>"
+
+
+def get_id(id):
+    """ Выводит кандидатов по их ID"""
+    id_list = ''
+    for candidates in load_candidate():
+        if candidates["id"] == id:
+            id_list += "<img src=https://picsum.photos/200>" + "\n" \
+                       "Имя кандидата: " + candidates["name"] + "\n" \
+                       "Позиция кандидата: " + candidates["position"] + "\n" \
+                       "Навыки через запятую: " + candidates["skills"] + "\n"
+            return "<h2>" + "<pre>" + id_list + "</pre>" + "<h2>"
+    return "<h1>" + "Нет такого кандидата" + "<h1>" + "<img src=https://picsum.photos/500>"
+
+
+def get_skil(skill):
+    """ Выводит кандидатов по их навыкам"""
+    skill_list = ""
+    for candidates in load_candidate():
+        skills = candidates["skills"].lower().split(', ')
+        if skill in skills:
+            skill_list += "Имя кандидата: " + candidates["name"] + " \n" \
+                          "Позиция кандидата: " + candidates["position"] + "\n" \
+                          "Навыки через запятую: " + candidates["skills"] + "\n""\n"
+    return "<h2>" + "<pre>" + skill_list + "</pre>" + "<h2>"
--- hw10/website.py
+++ hw10/website.py
@@ -1,46 +1,22 @@
 from flask import Flask
-from function import load_candidate
+from function import *
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def get_candidates():
-    """ Выведите полный список кандидатов """
-    candidate = ""
-    for candidates in load_candidate():
-        candidate += "Имя кандидата: " + candidates["name"] + " \n" \
-                     "Позиция кандидата: " + candidates["position"] + "\n" \
-                     "Навыки через запятую: " + candidates["skills"] + "\n""\n"
-    return "<h2>" + "<pre>" + candidate + "</pre>" + "<h2>"
+def candidate():
+    return get_candidates()
 
 
 @app.route("/candidates/<int:id>/")
-def get_id(id):
-    """ Выводит кандидатов по их ID"""
-    id_list = ''
-    for candidates in load_candidate():
-        if candidates["id"] == id:
-            id_list += "<img src=https://picsum.photos/200>" + "\n" \
-                       "Имя кандидата: " + candidates["name"] + "\n" \
-                       "Позиция кандидата: " + candidates["position"] + "\n" \
-                       "Навыки через запятую: " + candidates["skills"] + "\n"
-            return "<h2>" + "<pre>" + id_list + "</pre>" + "<h2>"
-    return "<h1>" + "Нет такого кандидата" + "<h1>" + "<img src=https://picsum.photos/500>"
-
+def id_candidate(id):
+    return get_id(id)
 
 @app.route("/skills/<skill>/")
-def get_skil(skill):
-    """ Выводит кандидатов по их навыкам"""
-    skill_list = ""
-    for candidates in load_candidate():
-        skills = candidates["skills"].lower().split(', ')
-        if skill in skills:
-            skill_list += "Имя кандидата: " + candidates["name"] + " \n" \
-                          "Позиция кандидата: " + candidates["position"] + "\n" \
-                          "Навыки через запятую: " + candidates["skills"] + "\n""\n"
-    return "<h2>" + "<pre>" + skill_list + "</pre>" + "<h2>"
-
+def candidate_skill(skill):
+    return get_skil(skill)
 
 if __name__ == "__main__":
     app.run(debug=True)
